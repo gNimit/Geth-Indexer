6797afb8f2265a10771e1f21a81da01708e2fed0
Nimit Gaur <nimitgaur1910@gmail.com>
2023-03-20 17:40:52 +0530

    Implemented Quit functionality.

--- indexer/upload.go
+++ indexer/upload.go
@@ -11,3 +11,11 @@ func upload(db *sql.DB, query string) {
 		log.Println("Failed to execute ", query, err)
 	}
 }
+
+func indexingCheck(db *sql.DB, relation string, column string) {
+	index := relation + "_" + column + "_" + "idx"
+	_, err := db.Exec("CREATE INDEX %s IF NOT EXISTS %s ON %s (%s) ", index, index, relation, column)
+	if err != nil {
+		log.Println(err)
+	}
+}
